03fc0c58686376d696ced0837a7ab6e97b4c9d1e
ChristianPommerenck <christian.pommerenck@gmail.com>
2025-03-21 17:46:04 -0300

    Update Foto.jsx

--- huellitas/src/Components/Detalle/Foto.jsx
+++ huellitas/src/Components/Detalle/Foto.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useState } from "react";
 
-const Foto = ({ imagen, tipoFoto, alt}) => {
-
+const Foto = ({ imagen, tipoFoto, alt }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  return (
 
-    <div className={tipoFoto}>    
-        {!isLoaded && <p>Cargando imagen...</p>}
-        <img className="foto"
-         src={imagen}
-          alt={alt}
-          onLoad={() => setIsLoaded(true)}
+  console.log(imagen); // Verifica si la URL es correcta
+
+  return (
+    <div className={tipoFoto}>
+      {!isLoaded && <p>Cargando imagen...</p>}
+      <img
+        className="foto"
+        src={imagen}
+        alt={alt}
+        onLoad={() => setIsLoaded(true)}
         style={{ display: isLoaded ? "block" : "none" }}
-        />
+      />
     </div>
   );
 };
+
 
 export default Foto;
